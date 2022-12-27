2d8a2872f6bc2fbef961a04cdf67ea508e356085
Whysoezzy <n.mamonovv@gmail.com>
2022-12-27 10:56:39 +0300

    Initial commit

---
+++ ServiceWeather/ServiceExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceWeather.Context;
+
+namespace ServiceWeather;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureCors(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy",
+                builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+        });
+    }
+    
+    public static void ConfigureIISIntegration(this IServiceCollection services)
+    {
+        services.Configure<IISOptions>(options =>
+        {
+
+        });
+    }
+    
+    public static void ConfigureMySqlContext(this IServiceCollection services, IConfiguration config) 
+    {
+        services.AddDbContext<WeatherContext>(o => o.UseInMemoryDatabase("weatherforecast"));
+    }
+}
